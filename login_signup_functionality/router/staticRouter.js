2bcff1426a205e956db627c9b4c040875a6186f8
TStushar <108743082+TStushar@users.noreply.github.com>
2024-07-21 19:52:33 +0530

    Cookie Auth added
    Login/Signup functionality with cookieAuthentication. 
    this helps to send 'req.user' from backend to frontend.

--- login_signup_functionality/router/staticRouter.js
+++ login_signup_functionality/router/staticRouter.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 
 const router = Router();
 router.get("/", (req, res) => {
+  console.log(req.user);
   res.render("home");
 });
 
--- login_signup_functionality/server.js
+++ login_signup_functionality/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const cookiePaser = require("cookie-parser");
+const { checkForAuthCookie } = require("./middleware/auth");
 
 const staticRouter = require("./router/staticRouter");
 const userRouter = require("./router/user");
@@ -13,6 +15,8 @@ app.set("views", path.resolve("./views"));
 
 // to handle data from form
 app.use(express.urlencoded({ extended: false }));
+app.use(cookiePaser()); //check for presence of user
+app.use(checkForAuthCookie("token")); //check for presence of user
 
 //connect with mongoDB database
 connectToTheDatabase("mongodb://127.0.0.1:27017/WriteWave");
